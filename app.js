64c721bcf1ba9aa819e81c849c1e2938b6a28909
thimappathesecond <hariharakumar08@gmail.com>
2025-09-04 21:06:09 +0530

    backend of prf project

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const authRouter = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
+//creating Express Application
+const app = express();
+app.use(bodyParser.json());
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://passwordresetflowapp.netlify.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Ensure cookies are sent
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+    ],
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/v1", authRouter);
+
+//Listen Request
+module.exports = app;
